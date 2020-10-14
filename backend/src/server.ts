57ce2a180e479ce410609c24ac32fc35692cfbbc
Gabriel de Souza Bonizário <gabrielbonizario98@gmail.com>
2020-10-14 14:40:25 -0300

    feat: add cors to allow cross origin access

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import path from 'path';
+import cors from 'cors';
 import './database/connection';
 
 import routes from './routes';
@@ -8,6 +9,7 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'tmp')));
